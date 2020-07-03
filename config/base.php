dd3d4e0de2b1ef955a56c229e691eae690ff3010
geochemistry <33366392+geochemistry@users.noreply.github.com>
2020-07-03 21:01:51 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,106 @@
+<?php return array (
+  'site_name' => 'od',
+  'password' => 'pkusess',
+  'style' => 'material',
+  'onedrive_root' => '/share/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'ipQEQ358^%jmkizZCVR31(~',
+  'client_id' => 'f0dc846d-79c0-4149-8721-280648513086',
+  'redirect_uri' => 'https://oneindex.github.io/',
+  'refresh_token' => 'OAQABAAAAAAAGV_bv21oQQ4ROqh0_1-tAQic3AH_rLhEqlgBcuKJKSq5A4ax5T4_mGqKl6JRyMCrySRWgjv03yMhlCEGtafHEPZp2njeKmv09q5kI9JBB20n5o8BT8m3ZVWfEADysQ3fVZeoOEggsPyzOruN0iCXoGVHmKX4vH_5E644oHbKpjpqq-KHB6sz_qh5ytF2mak5sYPyVWHShT5yU0XXW9xi0b1ngQiXsi47CvTB6ZY6QYG7C6arvwsaVS0fRP5N7_KJfFvi7WnMo0rEVTPkJGftmhXzdLrDvJoae7Hnj0Z6aEFF7I3PNJZ4o5obfzkLEVbGL48r9hFaOecI7y-2Ba5CB5TNwbXAumhqRJDAoHaoU4ZyuyQUwQosjSpLdVgerENtZJjq58acu8wfnWj2ybz0R7bW4jbVWR57SYYrY1DKFcVicqVGT5sISeriP1cppLS2YmERMAFbbu6BlSoYVAENcJe9xOu8VA2Yop73ewL5ZifTa7qqzoVNHYwZXa0NRyUrsOP4rA9fym6S5jwDEGsmSiG8pDyn8ZaUPmdVk86W7LbQIgWSdC39ZqRND0I1Ko3QBZMkamNbFByFUViFHnJjahpmkkndlcFY4qv1NXIOPWtz7PA-se-cJYsft9HsiLvgtIh46GEwPfpqAbN45A6ln6oFYeqtu_9tY6m-4iLaBKKY-OVxbc7A5NSvnsx-8Lbzz0XD2tD8UhOyjdM_EhRUFf77TLM83EfsSBFYRsPxuf5xl8BzlhwCS8N_Qx0E47ZiZYFtEwEQybj8Myv0BzbN6boMhwPV_ZblwFcMO01iP1UnkHjMd5SnrHkKIeCFqJapZ25TFEaYy8Iw1Z4WKR_2yqHA6L_BUkpRVAsMSdfJuRQBwi5ONmL084PZ6UKtMN_wt21yU4bRcZvjbXJg87SwEFkTDcXvzQHrWswe67CqTN2_Dj8nd-afQ3xUfojKO7agy2tHwIsUunVs-7wc30ZZBIAA',
+  'onedrive_hide' => '',
+  'onedrive_hotlink' => '',
+);
